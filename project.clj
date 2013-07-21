28ac988af8200430c9b583738f6515af8cb98b9f
rod <rod@pu-gh.com>
2013-07-21 20:00:40 +0100

    bump 0.4.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject rodnaph/cail "0.4.0"
+(defproject rodnaph/cail "0.4.1"
   :description "Sane javax.mail parser"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [javax.mail/mail "1.4.5"]])
