557d08d7444a16554f4c7c2847d3d44ea5622853
Rhodri Pugh <rod@pu-gh.com>
2013-07-22 12:11:43 +0100

    updated java mail version

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
 (defproject rodnaph/cail "0.4.1"
   :description "Sane javax.mail parser"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [javax.mail/mail "1.4.5"]])
+                 [javax.mail/mail "1.4.7"]])
 
