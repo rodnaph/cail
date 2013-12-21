32ba4eb3598b0b050dceb4fb11efe6113043c00a
Rhodri Pugh <rod@pu-gh.com>
2013-12-21 15:16:17 +0000

    bump 0.4.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject rodnaph/cail "0.4.2"
+(defproject rodnaph/cail "0.4.3"
   :description "Sane javax.mail parser"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [javax.mail/mail "1.4.7"]])
