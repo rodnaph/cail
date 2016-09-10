154ab43d28b71284ab92bff06d2ea0ad84fb5cd7
Rhodri Pugh <rod@pu-gh.com>
2016-09-10 20:39:13 +0100

    0.8.2

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject rodnaph/cail "0.8.1"
+(defproject rodnaph/cail "0.8.2"
   :description "Sane javax.mail parser"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [javax.mail/mail "1.4.7"]]
