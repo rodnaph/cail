99ac68180dcde32c3b75d0f19509411b16b316fa
Rhodri Pugh <rod@pu-gh.com>
2017-05-06 09:36:06 +0100

    update midje

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :dependencies [[cljfmt "0.5.6"]
                  [org.clojure/clojure "1.8.0"]
                  [javax.mail/mail "1.4.7"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
                    :plugins [[lein-cljfmt "0.5.6"]
-                             [lein-midje "3.1.1"]]}})
+                             [lein-midje "3.2.1"]]}})
